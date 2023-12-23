5c65ec10be3d513e0720781c10a1fc27554d7e56
BoltK1211 <alyxkellar09@gmail.com>
2023-12-23 00:15:41 -0500

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -15,7 +15,58 @@ Config.Anims = {
     [2] = {
         dict = "anim@heists@fleeca_bank@ig_7_jetski_owner",
         anim = "owner_idle",
-    }
-
+    },
+    [3] = {
+        dict = "rcm_barry3",
+        anim = "barry_3_sit_loop",
+    },
+    [4] = {
+        dict = "amb@world_human_picnic@male@idle_a",
+        anim = "idle_a",
+    },
+    [5] = {
+        dict = "amb@world_human_picnic@female@idle_a",
+        anim = "idle_a",
+    },
+    [6] = {
+        dict = "anim@heists@fleeca_bank@ig_7_jetski_owner",
+        anim = "owner_idle",
+    },
+    [7] = {
+        dict = "timetable@jimmy@mics3_ig_15@",
+        anim = "idle_a_jimmy",
+    },
+    [8] = {
+        dict = "anim@amb@nightclub@lazlow@lo_alone@",
+        anim = "lowalone_base_laz",
+    },
+    [9] = {
+        dict = "timetable@jimmy@mics3_ig_15@",
+        anim = "mics3_15_base_jimmy",
+    },
+    [10] = {
+        dict = "amb@world_human_stupor@male@idle_a",
+        anim = "idle_a",
+    },
+    [11] = {
+        dict = "timetable@ron@ig_5_p3",
+        anim = "ig_5_p3_base",
+    },
+    [12] = {
+        dict = "timetable@reunited@ig_10",
+        anim = "base_amanda",
+    },
+    [13] = {
+        dict = "timetable@ron@ig_3_couch",
+        anim = "base",
+    },
+    [14] = {
+        dict = "timetable@jimmy@mics3_ig_15@",
+        anim = "mics3_15_base_tracy",
+    },
+    [15] = {
+        dict = "timetable@maid@couch@",
+        anim = "base",
+    },
 }
 
