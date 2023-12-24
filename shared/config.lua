3a7423536ef2c2efa888a3496ce126efe2eac155
Jay <80186604+jay-fivem@users.noreply.github.com>
2023-12-24 23:07:20 +1030

    More Animations

--- shared/config.lua
+++ shared/config.lua
@@ -8,14 +8,37 @@ Config.SetToFirstPerson = true
 
 -- you can add more sitting animations here.
 Config.Anims = {
-    [1] = {
+    {
         dict = "timetable@maid@couch@",
         anim = "base",
     },
-    [2] = {
+    {
         dict = "anim@heists@fleeca_bank@ig_7_jetski_owner",
         anim = "owner_idle",
+    },
+    {
+        dict = "timetable@ron@ig_5_p3",
+        anim = "ig_5_p3_base",
+    },
+    {
+        dict = "timetable@reunited@ig_10",
+        anim = "base_amanda",
+    },
+    {
+        dict = "timetable@ron@ig_3_couch", 
+        anim = "base",
+    },
+    {
+        dict = "timetable@jimmy@mics3_ig_15@",
+        anim = "mics3_15_base_tracy",
+    },
+    {
+        dict = "timetable@ron@ron_ig_2_alt1", 
+        anim = "ig_2_alt1_base",
+    },
+    {
+        dict = "anim@gangops@morgue@table@", 
+        anim = "body_search",
     }
-
 }
 
